7ef2c314ff57da8323e7482cf453b48dbe74a162
Nguyen Duc Quan <ducquank52t1@gmail.com>
2023-10-11 22:01:31 +0700

    Welcome!

--- app/src/main/java/ducquan/id/vn/diceroller/MainActivity.kt
+++ app/src/main/java/ducquan/id/vn/diceroller/MainActivity.kt
@@ -1,8 +1,10 @@
 package ducquan.id.vn.diceroller
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -15,14 +17,40 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val dice = Dice(6)
-        val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceRoll: Pair<Int,Int>  = dice.roll()
+
+        var diceImage: ImageView = findViewById(R.id.imageView1)
+        when (diceRoll.first) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            else -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+
+        diceImage = findViewById(R.id.imageView2)
+        when (diceRoll.second) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            else -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+
+        fun showToast(context: Context, message: String) {
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
+
+        val message:String = if (diceRoll.first==diceRoll.second) "Luck! A pair of ${diceRoll.first}"
+                             else "${diceRoll.first+diceRoll.second} point! "
+        showToast(this, message)
     }
 }
 
 class Dice(private val numSides: Int) {
-    fun roll(): Int {
-        return (1..numSides).random()
+    fun roll(): Pair<Int,Int> {
+        return Pair((1..numSides).random(), (1..numSides).random())
     }
 }
